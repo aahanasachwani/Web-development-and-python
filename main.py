3b667f0267b1fe79cc91a0523e9c844cad443e72
aahanasachwani <aahanasachwani@gmail.com>
2025-01-08 14:24:54 +0530

    Intro to HTML

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import keyword
 print("sample")
 print(4)
 print("hi",5,"skjdhfo")
 print("Ajayanshi is really a cool girl\n\n")
 print("That was good to try",end= "&")
 print("Its done")
+
+name = "Payal"
+age = 27
+behaviour = input("Please type the behaviour of aahana ")
+print("My name is ",name," and I am ",age," years old","People think I behave like this ",behaviour)
+
+print("\n\nKeywords in python are \n \n")
+print(keyword.kwlist)
